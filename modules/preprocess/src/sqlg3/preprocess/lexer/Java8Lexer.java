54d7129585832abd0c36750c4455e6519231bf1c
Oleg Sobolev <sobolev_ov@mail.ru>
2021-09-30 17:22:19 +0300

    Updated dependencies versions

--- modules/preprocess/src/sqlg3/preprocess/lexer/Java8Lexer.java
+++ modules/preprocess/src/sqlg3/preprocess/lexer/Java8Lexer.java
@@ -1,4 +1,4 @@
-// Generated from grammar/Java8Lexer.g4 by ANTLR 4.7.2
+// Generated from grammar/Java8Lexer.g4 by ANTLR 4.9.2
 package sqlg3.preprocess.lexer;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class Java8Lexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.7.2", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =

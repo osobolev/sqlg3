c9a85de3d59b2cd1c8574ff3887cc94d9f773f9a
Oleg Sobolev <sobolev_ov@mail.ru>
2022-12-27 15:54:58 +0100

    Configuration for com.github.ben-manes.versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,30 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.43.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
+}
+
+fun getMajor(version: String, majorDepth: Int): String {
+    var p = -1;
+    for (i in 0 until majorDepth) {
+        p = version.indexOf('.', p + 1)
+        if (p < 0) break
+    }
+    return if (p < 0) "" else version.substring(0, p)
+}
+
+fun getMajorDepth(mod: ModuleComponentIdentifier): Int {
+    if (mod.group == "org.antlr") return 2
+    return 1
+}
+
+tasks.withType(com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask::class).configureEach {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val majorDepth = getMajorDepth(candidate)
+                if (getMajor(candidate.version, majorDepth) != getMajor(currentVersion, majorDepth)) {
+                    reject("Major update")
+                }
+            }
+        }
+    }
 }

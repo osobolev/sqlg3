9307b24af03f3e36f5d1abf7c5f15a1f9db2fabb
Oleg Sobolev <sobolev_ov@mail.ru>
2024-10-07 12:29:18 +0200

    Change default ifacePack

--- modules/preprocess/src/sqlg3/preprocess/OptionsBuilder.java
+++ modules/preprocess/src/sqlg3/preprocess/OptionsBuilder.java
@@ -22,7 +22,7 @@ public final class OptionsBuilder {
     public Path srcRoot = Paths.get(".");
 
     public Path destRoot;
-    public String ifacePack = ".";
+    public String ifacePack = "api";
 
     public Charset encoding = StandardCharsets.UTF_8;
     public int tabSize = 4;

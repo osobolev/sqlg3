0a051372013ecd11eeb1120d21a58a24585bd83a
Oleg Sobolev <sobolev_ov@mail.ru>
2024-10-07 11:07:31 +0200

    Minor fix

--- modules/preprocess/src/sqlg3/preprocess/ParseResult.java
+++ modules/preprocess/src/sqlg3/preprocess/ParseResult.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 final class ParseResult {
 
-    final String text;
+    private final String text;
     final HeaderResult header;
     final List<MethodEntry> entries;
     final Map<ParamName, List<ParamCutPaste>> bindMap;

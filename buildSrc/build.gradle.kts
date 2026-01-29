19ef21fe6d86fde36b2e4415c623a0266bd802a3
Oleg Sobolev <sobolev_ov@mail.ru>
2026-01-29 19:33:28 +0100

    Plugin update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("com.vanniktech:gradle-maven-publish-plugin:0.35.0")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.36.0")
 }
